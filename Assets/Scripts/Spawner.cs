0cddc602fd72e88212c2b04fa927160cf4467366
SknySir <nickyromano27@gmail.com>
2024-02-12 15:50:21 -0500

    Timer & Game Over added

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,25 +5,37 @@ using UnityEngine;
 public class spawner : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject[] AiPrefabs;
+    
+    /*/ public GameObject[] AiPrefabs;
     private float spawnRangeX = 30;
-    private float spawnPosZ = 20;
+    private float spawnPosZ = 20; /*/
+    public GameObject spawnee;
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
     void Start()
     {
-        
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
     }
 
+    public void SpawnObject()
+    {
+        Instantiate(spawnee, transform.position, transform.rotation);
+        if(stopSpawning)
+        {
+            CancelInvoke("spawnObject");
+        }
+    }
     // Update is called once per frame
   
-
-    void Update()
+    
+   /*/ void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
             Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
             int AiIndex= Random.Range(0,AiPrefabs.Length);
-            Instantiate(AiPrefabs[AiIndex], AiPrefabs[AiIndex].transform.position, AiPrefabs[AiIndex].transform.rotation);
+            Instantiate(AiPrefabs[AiIndex], AiPrefabs[AiIndex].transform.position, AiPrefabs[AiIndex].transform.rotation); /*/
         }
-    }
-}
+ 
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Romano-Minigame
  Minigame assignment
+Name of Game : Boundless
+
+Discription : This game is a simple move and shoot game where enemies spawn for 90 seconds and you must destroy as many as posible.. Enemies will be moving leftward and will exit and reapear on the right side of level. Your job is to destroy them with your projectile.
+
+How To Play : Move with "A" and "D", Shoot with "Space"
+
+Dev Log : I used basic movement features like adding a player controller to the users player allowing them to move left and right. 
+The player is locked between a -xRange and Xrange. enemies have a vector3.right continuously moving them with also a coninuous loop when interacting with the same -xRange and XRange. 
+The players projectile have a move forward script and can collide with the enemies, destroying them. There is a score feature that increases an int everytime you destroy an enemy with your projectile.
+There is a 90 second Timer that will take you to a game over scene with a button allowing you to start over.
